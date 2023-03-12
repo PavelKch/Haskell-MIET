f6a9156fceeadfdb09b168d6167e534798dc3aab
Pavel <korchaginlevap@gmail.com>
2023-03-12 21:02:51 +0300

    Дописан модуль FerstSteps, написаны тесты, не работают течты для rbgToCmyk

---
+++ Lab1/src/FirstSteps.hs
@@ -0,0 +1,101 @@
+module FirstSteps 
+where
+import Data.Word (Word8)
+
+-- xor x y находит "исключающее или" x и y
+-- xor True False == True
+-- xor True True == False
+
+-- используйте сопоставление с образцом
+xor :: Bool -> Bool -> Bool
+xor x y | (x || y) == False = False
+        | (x && y) == True  = False
+        | otherwise         = True
+
+-- max3 x y z находит максимум из x, y и z
+-- max3 1 3 2 == 3
+-- max3 5 2 5 == 5
+-- median3 x y z находит второе по величине число (медиану)
+-- median3 1 3 2 == 2
+-- median3 5 2 5 == 5
+max3, median3 :: Integer -> Integer -> Integer -> Integer
+max3 x y z = max (max y z) x
+
+median3 x y z | max3 x y z == x = max y z
+              | max3 x y z == y = max x z
+              | otherwise       = max x y
+
+-- Типы данных, описывающие цвета в моделях 
+-- RGB (https://ru.wikipedia.org/wiki/RGB), компоненты от 0 до 255
+-- и CMYK (https://ru.wikipedia.org/wiki/CMYK), компоненты от 0.0 до 1.0
+data RGB = RGB { red :: Word8, green :: Word8, blue :: Word8 } deriving (Eq, Show, Read)
+data CMYK = CMYK { cyan :: Double, magenta :: Double, yellow :: Double, black :: Double } deriving (Eq, Show, Read)
+-- Задайте функцию для их преобразования
+-- (формулы из http://www.codeproject.com/Articles/4488/XCmyk-CMYK-to-RGB-Calculator-with-source-code):
+-- Black   = min(1-Red, 1-Green, 1-Blue)
+-- Cyan    = (1-Red-Black) / (1-Black)
+-- Magenta = (1-Green-Black) / (1-Black)
+-- Yellow  = (1-Blue-Black) / (1-Black) 
+-- где значения Red, Green и Blue нормализованы от 0 до 1).
+
+-- Заметьте, что (/) для Int не работает, и неявного преобразования Int в Double нет.
+-- Это преобразование производится с помощью функции fromIntegral.
+rbgToCmyk :: RGB -> CMYK
+rbgToCmyk color = CMYK { cyan = cyan', magenta = magenta', yellow = yellow', black = black' } where 
+    green' = (fromIntegral (green color) :: Double) / 255
+    red'   = (fromIntegral (red color)   :: Double) / 255
+    blue'  = (fromIntegral (blue color)  :: Double) / 255
+    black' =  min (min (1 - red') (1 - green')) (1 - blue')
+    cyan'    =  if black' == 1 then 0 else (1 - red'   - black') / (1 - black')
+    magenta' =  if black' == 1 then 0 else (1 - green' - black') / (1 - black')
+    yellow'  =  if black' == 1 then 0 else (1 - blue'  - black') / (1 - black')
+	
+
+-- geomProgression b q n находит n-й (считая с 0) член 
+-- геометрической прогрессии, нулевой член которой -- b, 
+-- а знаменатель -- q.
+-- geomProgression 3.0 2.0 2 == 12.0
+
+-- используйте рекурсию
+-- не забудьте случаи n < 0 и n == 0.
+geomProgression :: Double -> Double -> Integer -> Double
+geomProgression b q n 
+    | n < 0     = error "The value of n must be greater than 0 !"
+    | otherwise = helper n b where 
+        helper 0 bn = bn
+        helper n bn = helper (n - 1) (bn * q)
+
+-- coprime a b определяет, являются ли a и b взаимно простыми
+-- (определение: Целые числа называются взаимно простыми, 
+-- если они не имеют никаких общих делителей, кроме +/-1)
+-- coprime 10 15 == False
+-- coprime 12 35 == True
+
+-- Используйте рекурсию
+-- Есть ли важные пограничные случаи или вспомогательные функции? Не забудьте добавить их в тесты.
+
+-- Полезные функции в Prelude (автоматически загруженной
+-- части стандартной библиотеки): quot, rem, quotRem 
+-- (или div, mod, divMod в зависимости от того, как 
+-- обрабатываете отрицательные числа)
+-- https://hackage.haskell.org/package/base-4.9.0.0/docs/Prelude.html
+
+
+
+coprime :: Integer -> Integer -> Bool
+coprime a b | gcd' a b == 1 = True
+            | otherwise     = False
+
+
+{- Алгоритм Евклида для нахожления НОД(a,b), 
+данная функция уже реализованна в Haskell, значение выдается по модулю -}  
+gcd' :: Integer -> Integer -> Integer 
+gcd' a b 
+    | (a == 0) && (b == 0) = 0
+    |  a == 0              = abs b
+    |  b == 0              = abs a
+    |  a == b              = abs a
+    | otherwise            = helper (abs a) (abs b)  where
+        helper a b = if mod (max a b) (min a b) == 0 
+		    then (min a b)
+	        else helper (min a b) (mod (max a b) (min a b))
---
+++ Lab1/test/Spec.hs
@@ -0,0 +1,85 @@
+import FirstSteps
+import Lists
+import Luhn
+import Test.Hspec
+import Control.Exception (evaluate)
+
+main :: IO ()
+main = hspec $ do
+    describe "first steps" $ do
+	-- Можно вложить глубже: describe "xor" do $ ... чтобы дать названия отдельным тестам
+        it "xor" $ do
+            xor True  True  `shouldBe` False
+            xor True  False `shouldBe` True
+            xor False True  `shouldBe` True
+            xor False False `shouldBe` False
+        it "max3" $ do
+            max3  1  2  3 `shouldBe` 3
+            max3  4  6  5 `shouldBe` 6
+            max3  9  7  8 `shouldBe` 9
+            max3 10 11 11 `shouldBe` 11
+            max3 13 12 13 `shouldBe` 13
+            max3 15 15 14 `shouldBe` 15
+            max3 16 16 16 `shouldBe` 16
+        it "median3" $ do
+            median3  1  2  3 `shouldBe` 2
+            median3  4  6  5 `shouldBe` 5
+            median3  8  7  9 `shouldBe` 8
+            median3 10 11 11 `shouldBe` 11
+            median3 13 12 13 `shouldBe` 13
+            median3 15 15 14 `shouldBe` 15
+            median3 16 16 16 `shouldBe` 16
+		
+        it "geomProgression" $ do
+            geomProgression 3.0 2.0 2 `shouldBe` 12.0
+            geomProgression 3.0 2.0 (-5) `shouldBe` error "The value of n must be greater than 0 !" -- с эти тестом проходит компиляцию, но тест проваливается ???
+			--geomProgression 3.0 2.0 (-5) `shouldBe` error "The value of n must be greater than 0 !" -- с эти тестом проходит компиляцию, но тест проваливается ???
+			--evaluate (geomProgression 3.0 2.0 (-5)) `shouldBe` error "The value of n must be greater than 0 !"
+            geomProgression 3.0 2.0 0 `shouldBe` 3.0
+            geomProgression 1.0 1.0 100000 `shouldBe` 1.0
+            geomProgression 1.0 0.5 10 `shouldBe` 9.765625e-4 --  1.0 / (2 ^ 10) == 9.765625e-4
+            geomProgression 1.0 2.0 40 `shouldBe` 1.099511627776e12 --  2^40 == 1099511627776
+            geomProgression 1.0 0.9 100 `shouldBe` 2.6561398887587544e-5
+            geomProgression (10.0 ^ 100) 0.1 100 `shouldBe` 1.0
+			
+        it "coprime" $ do
+    	    coprime 10 15 `shouldBe` False
+            coprime 12 35 `shouldBe` True		    
+            coprime 10 (-15) `shouldBe` False
+            coprime (-12) 35 `shouldBe` True
+            coprime (-12) (-35) `shouldBe` True
+            coprime 269 271 `shouldBe` True			
+{- так как существует реализация данной функции, проверять буду с ней -}			
+        it "gcd'" $ do
+            gcd' 0 0 `shouldBe` gcd 0 0
+            gcd' 1 1 `shouldBe` gcd 1 1
+            gcd' 0 8 `shouldBe` gcd 0 8
+            gcd' 3 0 `shouldBe` gcd 3 0
+            gcd' 1 2 `shouldBe` gcd 1 2
+            gcd' 10 100 `shouldBe` gcd 10 100
+            gcd' 100 10 `shouldBe` gcd 10 100
+            gcd' 101 400001 `shouldBe` gcd 101 400001
+            gcd' 2424527370 75767567670 `shouldBe` gcd 2424527370 75767567670 			
+            gcd' 269 271 `shouldBe` gcd 269 271 		
+			
+-- Пока не получилось отладить тесты для rbgToCmyk	
+{-		it "rbgToCmyk" $ do
+           (rbgToCmyk (RGB 0 0 0)) `shouldBe` CMYK {cyan = 0.0, magenta = 0.0, yellow = 0.0, black = 1.0}
+           (rbgToCmyk (RGB 255 255 255)) `shouldBe` CMYK {cyan = 0.0, magenta = 0.0, yellow = 0.0, black = 0.0}
+           (rbgToCmyk (RGB 10 10 10)) `shouldBe` CMYK {cyan = 0.0, magenta = 0.0, yellow = 0.0, black = 0.96}
+           (rbgToCmyk (RGB 100 50 25)) `shouldBe` CMYK {cyan = 0.0, magenta = 0.50, yellow = 0.75, black = 0.6}
+           (rbgToCmyk (RGB 180 230 13)) `shouldBe` CMYK {cyan = 0.22, magenta = 0.0, yellow = 0.94, black = 0.1} -} 
+
+    describe "lists" $ do
+        it "distance" pending
+        it "intersect" pending
+        it "zipN" pending
+        it "find" pending
+        it "findLast" pending
+        it "mapFuncs" pending
+        it "tailNel" pending
+        it "lastNel" pending
+        it "zipNel" pending
+        it "listToNel" pending
+        it "nelToList" pending
+    describe "luhn" $ it "" pending
